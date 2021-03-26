656c3d7bbf1d52cf93cd2a1a60a9190a9a786ee0
Reinier Koops <info@reinier.work>
2021-03-26 17:48:41 +0000

    Implemented 1/3 to 2/3 epoch for Learning rates

--- datasets/CFD/CFDdata.py
+++ datasets/CFD/CFDdata.py
@@ -2,10 +2,11 @@ import os
 import glob
 import pathlib
 import random
-
+import torch
 import numpy as np
 from torch.utils.data import Dataset
 from PIL import Image
+
 
 class CFD(Dataset):
     def __init__(self, img_dir, img_transform, mask_dir, mask_transform):
@@ -17,13 +18,13 @@ class CFD(Dataset):
         self.mask_fnames = glob.glob(os.path.join(mask_dir, "*.png"))
         self.mask_transform = mask_transform
 
-        self.seed = np.random.randint(2147483647)
+        self.seed = torch.manual_seed(15)
 
     def __getitem__(self, i):
         fpath = self.img_fnames[i]
         img = Image.open(fpath).convert("RGB")
         if self.img_transform is not None:
-            random.seed(self.seed)
+            self.seed
             img = self.img_transform(img)
 
         mpath = self.mask_fnames[i]
@@ -37,11 +38,11 @@ class CFD(Dataset):
         return len(self.img_fnames)
 
 
-if __name__ == "__main__":
-    img_dir = pathlib.Path("cfd_image")
-    img_transform = None
+# if __name__ == "__main__":
+#     img_dir = pathlib.Path("cfd_image")
+#     img_transform = None
 
-    mask_dir = pathlib.Path("seg_gt")
-    mask_transform = None
-    img, mask = CFD(img_dir, img_transform, mask_dir, mask_transform)[0]    # get the first img, mask tuplet
-    print(f"image shape: {img.size}\nmask shape: {mask.size}")
+#     mask_dir = pathlib.Path("seg_gt")
+#     mask_transform = None
+#     img, mask = CFD(img_dir, img_transform, mask_dir, mask_transform)[0]    # get the first img, mask tuplet
+#     print(f"image shape: {img.size}\nmask shape: {mask.size}")
--- utils/layers.py
+++ utils/layers.py
@@ -1,12 +1,9 @@
-from collections import OrderedDict
-
-
 def layer_split(network):
     # Tuning the learning rates
     # Split into three groups of layers, to have three different learning rates
-    layer_1 = OrderedDict()
-    layer_2 = OrderedDict()
-    layer_3 = OrderedDict()
+    layer_1 = []
+    layer_2 = []
+    layer_3 = []
     layer_1_to_include = [
         "blueBlock",
         "resBlock1",
@@ -19,10 +16,10 @@ def layer_split(network):
 
     for name, param in network.named_parameters():
         if any(substring in name for substring in layer_1_to_include):
-            layer_1[name] = param
+            layer_1.append(param)
         elif any(substring in name for substring in layer_2_to_include):
-            layer_1[name] = param
+            layer_2.append(param)
         else:
-            layer_3[name] = param
+            layer_3.append(param)
 
     return layer_1, layer_2, layer_3
