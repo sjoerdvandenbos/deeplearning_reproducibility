0e39d8f4152ed71d0f4f18f76038ee0b77eaec33
Reinier Koops <info@reinier.work>
2021-03-29 09:07:00 +0000

    Fixed dataloader & model

--- datasets/CFD/CFDdata.py
+++ datasets/CFD/CFDdata.py
@@ -11,24 +11,23 @@ from PIL import Image
 class CFD(Dataset):
     def __init__(self, img_dir, img_transform, mask_dir, mask_transform):
         self.img_dir = img_dir
-        self.img_fnames = glob.glob(os.path.join(img_dir, "*.jpg"))
+        self.img_fnames = sorted(glob.glob(os.path.join(img_dir, "*.jpg")))
         self.img_transform = img_transform
 
         self.mask_dir = mask_dir
-        self.mask_fnames = glob.glob(os.path.join(mask_dir, "*.png"))
+        self.mask_fnames = sorted(glob.glob(os.path.join(mask_dir, "*.png")))
         self.mask_transform = mask_transform
 
-        self.seed = torch.manual_seed(15)
-
-    def __getitem__(self, i):
+    def __getitem__(self, i: int):
         fpath = self.img_fnames[i]
         img = Image.open(fpath).convert("RGB")
+        state = torch.get_rng_state()
         if self.img_transform is not None:
-            self.seed
             img = self.img_transform(img)
 
         mpath = self.mask_fnames[i]
         mask = Image.open(mpath).convert("1")
+        torch.set_rng_state(state)
         if self.mask_transform is not None:
             mask = self.mask_transform(mask)
 
