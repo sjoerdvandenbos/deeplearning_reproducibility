b8bf138fcc0994f50cbb41938f65bc0ac3818767
Reinier Koops <info@reinier.work>
2021-03-31 08:46:59 +0000

    Created new accuracy measure

--- loss/loss.py
+++ loss/loss.py
@@ -24,33 +24,6 @@ def batch_dice_loss(true_val, pred_val, epsilon=1e-8):
 
     return torch.mean(1 - ((numerator + epsilon) / (denominator + epsilon)))
 
-
-def dice_loss(true_val, pred_val, epsilon=1e-8):
-    """
-    Dice coefficient loss
-    ---
-    Equivalent to F1 score. Often used when there is
-    imbalance in dataset (non-crack pixels outnumber
-    crack pixels by 65:1).
-
-    Args:
-        true_val: a tensor of shape [N, 1, H, W]
-        predicted_val: a tensor of shape [N, 1, H, W]
-    Returns:
-        dice_loss: the Dice loss.
-    """
-    # Sigmoid -> [0, 1], reflect probabilities
-    pred_val = (torch.sigmoid(pred_val) >= 0.5).float()
-
-    # Flattened from [H, W] to [H*W]
-    true_val = true_val.flatten()
-    pred_val = pred_val.flatten()
-
-    numerator = 2. * (pred_val * true_val).sum()
-    denominator = (pred_val).sum() + (true_val).sum()
-
-    return torch.mean(1 - ((numerator + epsilon) / (denominator + epsilon)))
-
 #
 # prediction = torch.randint(low=-255, high=256, size=(3, 3), dtype=torch.float)
 
